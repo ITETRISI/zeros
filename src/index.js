3a71246601f1f62bfa31374c069a07dd1b1abd4c
Daniil Asipenka <ocipenkoden@gmail.com>
2019-10-01 00:06:58 +0300

    feat: complete task zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+    var arr = expression.split('*')
+    var integral = 1;
+    for (let i in arr) {
+        if (arr[i].indexOf('!!') == -1) {
+            arr[i] = arr[i].slice(0, -1);
+            for (let y = 1; y <= arr[i]; y++) {
+                integral = BigInt(integral) * BigInt(y);
+            }
+        } else {
+            arr[i] = arr[i].slice(0, -2);
+            if (arr[i] % 2 == 0) {
+                for (let y = 2; y <= arr[i]; y += 2) {
+                    integral = BigInt(integral) * BigInt(y);
+                }
+            } else {
+                for (let y = 1; y <= arr[i]; y += 2) {
+                    integral = BigInt(integral) * BigInt(y);
+                }
+            }
+        }
+    }
+    var zero = true;
+    var zeros = [...integral + ''].reduceRight((sum, number) => {
+        (number == '0' && zero) ? sum++ : zero = false;
+        return sum;
+    }, 0);
+    return zeros;
+};
